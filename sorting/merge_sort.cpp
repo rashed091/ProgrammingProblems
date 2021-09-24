3d01ac34790c19852462aa31d158261507dbb495
mrasheduzzaman <rashed091nsu@gmail.com>
2021-09-24 20:20:06 +0600

    Mergesort implementation

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -3,30 +3,52 @@ using namespace std;
 
 void merge(vector<int> &A, int l, int m, int r)
 {
-    vector<int> result;
-    int j = r;
-    for (size_t i = l; i < m; ++i)
+    int i, j, k;
+    vector<int> left;
+    vector<int> right;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    for (i = 0; i < n1; ++i)
     {
-        if (j == m || (l < r && A[l] <= A[r]))
+        left.push_back(A[l + i]);
+    }
+    for (j = 0; j < n2; ++j)
+    {
+        right.push_back(A[m + 1 + j]);
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 || j < n2)
+    {
+        if (j >= n2 || (i < n1 && left[i] <= right[j]))
         {
-            result.push_back(A[l++]);
+            A[k] = left[i];
+            i++;
         }
         else
         {
-            result.push_back(A[r++]);
+            A[k] = right[j];
+            j++;
         }
-    }
-    for (size_t i = l; i < result.size(); ++i)
-    {
-        A[i] = result[i];
+        k++;
     }
 }
+
+void merge(vector<int>& A, int l, int m, int r) {
+    
+}
+
 
 void mergesort(vector<int> &A, int l, int r)
 {
     if (l < r)
     {
         int m = floor((l + r) / 2);
+        cout << "left:" << l << " m:" << m << " r: " << r << endl;
         mergesort(A, l, m);
         mergesort(A, m + 1, r);
         merge(A, l, m, r);
